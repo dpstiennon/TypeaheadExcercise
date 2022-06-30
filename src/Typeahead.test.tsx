4c7f53d97bcb5d3e465c5c4322c98b0b35d92ac7
dpstiennon <david.stiennon@gmail.com>
2022-06-30 09:19:37 -0500

    A bunch of tests

--- src/Typeahead.test.tsx
+++ src/Typeahead.test.tsx
@@ -4,14 +4,8 @@ import Typeahead from "./Typeahead";
 import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom/extend-expect";
 
-// test("renders learn react link", async () => {
-//   // const linkElement = getByText(/learn react/i);
-//   const { getByRole } = render(<Typeahead list={["thing"]} />);
-//   // render(<Typeahead list={["thing"]} />);
-//   const input = getByRole("textbox");
-//   await userEvent.type(input, 'c')
-//   expect(input).
-// });
+//Note - had a devil of a time getting tests running with dependency incompatibilities.
+//Eventually just update all dependencies to latest version
 
 describe("<Typeahead>", () => {
   it("should show blank textbox by default", () => {
@@ -21,12 +15,41 @@ describe("<Typeahead>", () => {
     expect(screen.queryAllByRole("button")).toHaveLength(0);
   });
 
-  it("should show a dropdown list if you enter text", () => {});
-
-  it("should return false", () => {
-    render(<Typeahead list={["thing"]} />);
+  it("should show a dropdown list if you enter text", async () => {
+    render(<Typeahead list={["thing1", "thing2"]} />);
     const input = screen.getByRole("textbox");
-    expect(input).toBeInTheDocument();
-    expect(false).toBeFalsy();
+    await userEvent.type(input, "t");
+    expect(screen.queryAllByRole("button")).toHaveLength(2);
+  });
+
+  it("should show no list if no matching entries", async () => {
+    render(<Typeahead list={["thing1", "thing2"]} />);
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "xxxx");
+    expect(screen.queryAllByRole("button")).toHaveLength(0);
+  });
+
+  it("should show the dropdown even if leading space and case mismatch", async () => {
+    render(<Typeahead list={["thing1", "thing2"]} />);
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "   Thi");
+    expect(screen.queryAllByRole("button")).toHaveLength(2);
+  });
+
+  it("should populate the textbox if a list item clicked", async () => {
+    render(<Typeahead list={["thing1", "thing2"]} />);
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "th");
+    await userEvent.click(screen.queryAllByRole("button")[0]);
+    expect(input).toHaveValue("thing1");
+    expect(screen.queryAllByRole("button")).toHaveLength(0);
+  });
+
+  it("should tab between options", async () => {
+    render(<Typeahead list={["thing1", "thing2"]} />);
+    const input = screen.getByRole("textbox");
+    await userEvent.type(input, "th");
+    userEvent.tab();
+    expect(screen.queryAllByRole("button")[0]).toHaveFocus();
   });
 });
