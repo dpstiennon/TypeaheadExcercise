381586985421b3f43f5a13630b39de1d782bf537
dpstiennon <david.stiennon@gmail.com>
2022-06-30 09:03:29 -0500

    Got testing setup working tinkering with package.json versions

--- src/Typeahead.test.tsx
+++ src/Typeahead.test.tsx
@@ -1,27 +1,32 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Typeahead from "./Typeahead";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
 
-// test("renders learn react link", () => {
+// test("renders learn react link", async () => {
 //   // const linkElement = getByText(/learn react/i);
-//   // const { getByText } = render(<Typeahead list={["thing"]} />);
-//   render(<Typeahead list={["thing"]} />);
-//   const input = screen.getByRole("textbox");
-//   expect(input).toBeInTheDocument();
+//   const { getByRole } = render(<Typeahead list={["thing"]} />);
+//   // render(<Typeahead list={["thing"]} />);
+//   const input = getByRole("textbox");
+//   await userEvent.type(input, 'c')
+//   expect(input).
 // });
 
-describe("re", () => {
-  it("should return true", () => {
+describe("<Typeahead>", () => {
+  it("should show blank textbox by default", () => {
     render(<Typeahead list={["thing"]} />);
     const input = screen.getByRole("textbox");
     expect(input).toBeInTheDocument();
-    expect(true).toBeFalsy();
+    expect(screen.queryAllByRole("button")).toHaveLength(0);
   });
+
+  it("should show a dropdown list if you enter text", () => {});
 
   it("should return false", () => {
     render(<Typeahead list={["thing"]} />);
     const input = screen.getByRole("textbox");
     expect(input).toBeInTheDocument();
-    expect(true).toBeFalsy();
+    expect(false).toBeFalsy();
   });
 });
